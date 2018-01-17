7af2c2353d5da06ff92ba965074386ed6492eeba
Omar Bin Salamah <Omaraz@users.noreply.github.com>
2018-01-17 12:45:41 -0600

    Update AgeCalculator.py

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -1,16 +1,6 @@
 # Author:  OMAR BIN SALAMAH
 # Version: 2017-10-4
 
-## # # # # # # # # # # # # # # # # # # # # # # # # # # # # ##
-# Description: program that uses an algorithm to determine  #
-# the day of the week a person was born based on their DOB  #
-# program scrapes Wikipedia and pulls a summary of the year #
-# program scrapes worldhistoryproject.org and pulls what    #
-# happened that year. Program tweets, emails and Facebook   #
-# posts the result. Program prints the results, saves the   #
-# results as PDF or as a screen shot!                       #
-## # # # # # # # # # # # # # # # # # # # # # # # # # # # # ##
-
 import os
 import random
 import sys
